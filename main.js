3647b2120e6431d7acce77b68e202910f8d7fe85
Beartil Rajan <229802592+beartilrajan@users.noreply.github.com>
2026-02-06 16:39:53 +0000

    Remove button and popup enhanced

--- main.js
+++ main.js
@@ -13,11 +13,23 @@ function addBookToLibrary() {
 const popup = document.getElementById("popup");
 const opnbtn = document.querySelector(".add");
 const cancel = document.querySelector(".Cancel");
+const container = document.querySelector(".container")
+const remove = document.querySelector(".remove")
+const content = document.querySelector(".content")
 
 opnbtn.addEventListener("click", () => {
   popup.style.display = "flex";
+  container.style.filter = "blur(6px)"
 });
 
 cancel.addEventListener("click", () => {
   popup.style.display = "none";
+  container.style.filter = "blur(0px)"
 });
+
+content.addEventListener("click", (e) => {
+  if (e.target.classList.contains("remove")) {
+    const cardClicked = e.target.closest(".card");
+    cardClicked.remove();
+  }
+})
