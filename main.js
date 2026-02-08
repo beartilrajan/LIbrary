fe01618948054eb0424352cf944fc6724b8ed39f
Beartil Rajan <229802592+beartilrajan@users.noreply.github.com>
2026-02-08 15:16:26 +0000

    book function and display added using dom

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+const popup = document.getElementById("popup");
+const opnbtn = document.querySelector(".add");
+const cancel = document.querySelector(".Cancel");
+const container = document.querySelector(".container");
+const remove = document.querySelector(".remove");
+const content = document.querySelector(".content");
+const addBook = document.querySelector(".Add-Book");
 
 const myLibrary = [];
 
@@ -10,21 +17,41 @@ function Book(title, author, pages, isRead) {
 }
 
 function addBookToLibrary(title, author, pages, isRead = false) {
-  const newbook = new Book(title, author, pages, isRead)
-  myLibrary.push(newbook);
+  const newBook = new Book(title, author, pages, isRead)
+  myLibrary.push(newBook);
   displaybook();
 }
 
+
+
 function displaybook() {
-  
+  content.innerHTML = "";
+  myLibrary.forEach(book => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.dataset.id = book.id;
+    card.innerHTML = `<div class="title">${book.title}</div>
+                <div class="subCard">
+                    <div class="author">
+                        <span id="authorLabel">Author:</span>
+                        <span id="authorName">${book.author}</span>
+                    </div>
+                    <div class="pages">
+                        <span id="pageLabel">Page: </span>
+                        <span id="pageNumber">${book.pages}</span>
+                    </div>
+                </div>
+                <div class="buttons">
+                    <button class="changeStatus">${book.isRead ? "Mark Unread" : "Mark Read"}</button>
+                    <button class="remove">Remove</button>
+                </div>`
+      
+    content.append(card);
+  });
 }
 
-const popup = document.getElementById("popup");
-const opnbtn = document.querySelector(".add");
-const cancel = document.querySelector(".Cancel");
-const container = document.querySelector(".container")
-const remove = document.querySelector(".remove")
-const content = document.querySelector(".content")
+
+
 
 
 opnbtn.addEventListener("click", () => {
@@ -60,14 +87,19 @@ content.addEventListener("click", (e) => {
   }
 })
 
-const addBook = document.querySelector(".Add-Book")
 
 popup.addEventListener("click", (e) => {
   if (e.target.classList.contains("Add-Book")) {
+    e.preventDefault();
+
     const title = document.getElementById("Title").value;
     const author = document.getElementById("Author").value;
     const pages = document.getElementById("Pages").value;
-    addBookToLibrary(title, author, pages);
+    addBookToLibrary(title, author, pages, false);
+    popup.style.display = "none";
+    container.style.filter = "blur(0px)";
   }
 })
 
+addBookToLibrary("Atomic Habits", "James Clear", 320, true);
+addBookToLibrary("Deep Work", "Cal Newport", 296, false);
