8e0e43bf1ded18b171b36b570babf473f58c2389
Beartil Rajan <229802592+beartilrajan@users.noreply.github.com>
2026-02-08 12:13:50 +0000

    new commit

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
 
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, pages, isRead) {
+  this.id = crypto.randomUUID();
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = isRead;
 }
 
-function addBookToLibrary() {
-  // take params, create a book then store it in the array
+function addBookToLibrary(title, author, pages, isRead = false) {
+  const newbook = new Book(title, author, pages, isRead)
+  myLibrary.push(newbook);
+  displaybook();
 }
 
+function displaybook() {
+  
+}
 
 const popup = document.getElementById("popup");
 const opnbtn = document.querySelector(".add");
@@ -16,6 +25,7 @@ const cancel = document.querySelector(".Cancel");
 const container = document.querySelector(".container")
 const remove = document.querySelector(".remove")
 const content = document.querySelector(".content")
+
 
 opnbtn.addEventListener("click", () => {
   popup.style.display = "flex";
@@ -33,3 +43,31 @@ content.addEventListener("click", (e) => {
     cardClicked.remove();
   }
 })
+
+
+content.addEventListener("click", (e) => {
+  if (e.target.classList.contains("changeStatus")) {
+    const card = e.target.closest(".card");
+    const button = e.target;
+    if (button.textContent === "Mark Unread") {
+      card.style.backgroundColor = "#909090";
+      button.textContent = "Mark Read";
+    }
+    else if (button.textContent === "Mark Read") {
+      card.style.backgroundColor = "#c9ff87";
+      button.textContent = "Mark Unread";
+    }
+  }
+})
+
+const addBook = document.querySelector(".Add-Book")
+
+popup.addEventListener("click", (e) => {
+  if (e.target.classList.contains("Add-Book")) {
+    const title = document.getElementById("Title").value;
+    const author = document.getElementById("Author").value;
+    const pages = document.getElementById("Pages").value;
+    addBookToLibrary(title, author, pages);
+  }
+})
+
